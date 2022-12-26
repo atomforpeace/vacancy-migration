6408ecceb41fa23c97eafe193d92c7f860ef1c05
Anton Lushnikov <antonlushnikow@gmail.com>
2022-12-26 22:16:36 +0500

    Add surface concentration

--- vacancy/manage.py
+++ vacancy/manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vacancy.settings')
+    if os.path.exists("vacancy//settings_dev.py"):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vacancy.settings_dev")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vacancy.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
