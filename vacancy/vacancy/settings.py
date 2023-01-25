9cd70026697084ae7c877607212d825f3683f3a3
Anton Lushnikov <antonlushnikow@gmail.com>
2023-01-25 23:30:29 +0500

    add server ip

--- vacancy/vacancy/settings.py
+++ vacancy/vacancy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['194.87.237.4']
 
 
 # Application definition
