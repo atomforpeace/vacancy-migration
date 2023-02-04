785c1917072155b978bd2b414f94c76d8bb54aa0
atomforpeace <112865284+atomforpeace@users.noreply.github.com>
2023-02-04 18:36:38 +0500

    Update settings.py

--- vacancy/vacancy/settings.py
+++ vacancy/vacancy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['194.87.237.4']
+ALLOWED_HOSTS = ['172.16.1.1']
 
 
 # Application definition
